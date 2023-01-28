6c3903c24615109756d11c8a7c05bad8b3f7ad9c
allenmurfee <allenmurfee123@gmail.com>
2023-01-28 18:24:05 -0500

    set up components

---
+++ client/src/components/AddNew.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AddNew() {
+  return (
+    <div class="new-todo">
+      <h3>Add a new to-do!</h3>
+      <button class="submit" type="submit">
+        Add
+      </button>
+    </div>
+  );
+}
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,37 +2,38 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
-function Nav() {
-
-  function showLoginButton() {
-    if (Auth.loggedIn()) {
-      return (
-        
-        <div class="navbar">
-            <button class="button">Profile</button>
-            <button class="button" onClick={() => Auth.logout()}>Logout</button>
+export default function Header() {
+  if (Auth.loggedIn()) {
+    return (
+      <header class="header">
+        <div>
+          <h1 class="title">To-do List App</h1>
+          <h3>Turn your to-do's into to-done!</h3>
         </div>
-      );
-    } else {
-      return (
         <div class="navbar">
-            <button class="button"><Link to="/login">Login</Link></button>
-            <button class="button"><Link to="/signup">Signup</Link></button>
+          <button class="button">Profile</button>
+          <button class="button" onClick={() => Auth.logout()}>
+            Logout
+          </button>
         </div>
-      );
-    }
+      </header>
+    );
+  } else {
+    return (
+      <header class="header">
+        <div>
+          <h1 class="title">To-do List App</h1>
+          <h3>Turn your to-do's into to-done!</h3>
+        </div>
+        <div class="navbar">
+          <button class="button">
+            <Link to="/login">Login</Link>
+          </button>
+          <button class="button">
+            <Link to="/signup">Signup</Link>
+          </button>
+        </div>
+      </header>
+    );
   }
-
-  return (
-    <header class="header">
-      <div>
-        <h1 class="title">To-do List App</h1>
-        <h3>Turn your to-do's into to-done!</h3>
-      </div>
-      {showLoginButton()}
-      
-    </header>
-  );
 }
-
-export default Nav;
