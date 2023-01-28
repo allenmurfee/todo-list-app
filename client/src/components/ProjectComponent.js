51ea108c90f5f5ebd8dab3e50ad629caa4bf6875
Brendan Shelton <brendan.d.shelton@gmail.com>
2023-01-28 18:03:33 -0500

    worked on React components

---
+++ client/src/components/ProjectComponent.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function ProjectComponent() {
+
+  
+    return (
+        <div class="container">
+        <section class="card start">
+          <p>Haven't Started</p>
+          <ol>
+            <li class="list-item">Final essay draft.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Move to 'In Progress'">></button>
+              <button class="list-button" title="Delete">X</button>
+            </li>
+            <li class="list-item">Organize project research and visuals.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Move to 'In Progress'">></button>
+              <button class="list-button" title="Delete">X</button></li>
+            <li class="list-item"> Create powerpoint presentation.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Move to 'In Progress'">></button>
+              <button class="list-button" title="Delete">X</button></li>
+          </ol>
+        </section>
+        <section class="card progress">
+          <p>In Progress</p>
+          <ol>
+            <li class="list-item">Interview 3 people.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Move to 'Done'">></button>
+              <button class="list-button" title="Delete">X</button></li>
+            <li class="list-item">Second essay draft
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Move to 'Done'">></button>
+              <button class="list-button" title="Delete">X</button>.</li>
+          </ol>
+        </section>
+        <section class="card done">
+          <p>Done</p>
+          <ol>
+            <li class="list-item">Create project outline.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Delete">X</button>
+            </li>
+            <li class="list-item">Essay rough draft.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Delete">X</button>
+            </li>
+            <li class="list-item">Gather research materials.
+              <button class="list-button" title="Edit">/</button>
+              <button class="list-button" title="Delete">X</button>
+            </li>
+          </ol>
+        </section>
+      </div>
+    );
+  }
+  
+  export default ProjectComponent;
