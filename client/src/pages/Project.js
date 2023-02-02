91a75fd7fc5e9ecfe3975e19643a42de510518f6
Brendan Shelton <brendan.d.shelton@gmail.com>
2023-02-01 22:04:33 -0500

    added query

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,103 +1,93 @@
 import React from "react";
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
 
 function ProjectComponent() {
+  const { data } = useQuery(QUERY_USER);
+  let user = data?.user || {}
+
   return (
-    <div class="container">
-      <section class="card start">
-        <p>Haven't Started</p>
-        <ol>
-          <li class="list-item">
-            Final essay draft.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Move to 'In Progress'"></button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-          <li class="list-item">
-            Organize project research and visuals.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Move to 'In Progress'"></button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-          <li class="list-item">
-            {" "}
-            Create powerpoint presentation.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Move to 'In Progress'"></button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-        </ol>
-      </section>
-      <section class="card progress">
-        <p>In Progress</p>
-        <ol>
-          <li class="list-item">
-            Interview 3 people.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Move to 'Done'"></button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-          <li class="list-item">
-            Second essay draft
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Move to 'Done'"></button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-            .
-          </li>
-        </ol>
-      </section>
-      <section class="card done">
-        <p>Done</p>
-        <ol>
-          <li class="list-item">
-            Create project outline.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-          <li class="list-item">
-            Essay rough draft.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-          <li class="list-item">
-            Gather research materials.
-            <button class="list-button" title="Edit">
-              /
-            </button>
-            <button class="list-button" title="Delete">
-              X
-            </button>
-          </li>
-        </ol>
-      </section>
-    </div>
+      user.projects.map((project) =>{
+         let notStartedToDos = [];
+         let inProgressTodos = [];
+         let finishedToDos = [];
+        
+        for (let toDo in project.toDos) {
+          switch (toDo.status){
+            case 'notStarted':
+              notStartedToDos.push(toDo);
+              break;
+            case 'inProgress':
+              inProgressTodos.push(toDo);
+              break;
+            case 'finished':
+              finishedToDos.push(toDo);
+              break;
+          }
+        }
+        return (
+          <div class="container">
+          <section class="card start">
+          <p>Haven't Started</p>
+          <ol>
+            {notStartedToDos.map((toDo) =>{
+              return (
+            <li class="list-item">
+              {toDo.description}
+              <button class="list-button" title="Edit">
+                /
+              </button>
+              <button class="list-button" title="Move to 'In Progress'"></button>
+              <button class="list-button" title="Delete">
+                X
+              </button>
+            </li>
+              )
+            })}
+          </ol>
+        </section>
+        <section class="card progress">
+          <p>In Progress</p>
+          <ol>
+            {inProgressToDos.map((toDo) =>{
+              return (
+            <li class="list-item">
+              {toDo.description}
+              <button class="list-button" title="Edit">
+                /
+              </button>
+              <button class="list-button" title="Move to 'In Progress'"></button>
+              <button class="list-button" title="Delete">
+                X
+              </button>
+            </li>
+              )
+            })}
+          </ol>
+        </section>
+        <section class="card done">
+          <p>Done</p>
+          <ol>
+            {finishedToDos.map((toDo) =>{
+              return (
+            <li class="list-item">
+              {toDo.description}
+              <button class="list-button" title="Edit">
+                /
+              </button>
+              <button class="list-button" title="Move to 'In Progress'"></button>
+              <button class="list-button" title="Delete">
+                X
+              </button>
+            </li>
+              )
+            })}
+          </ol>
+        </section>
+      </div>
+        )
+      })
+      
   );
 }
 
